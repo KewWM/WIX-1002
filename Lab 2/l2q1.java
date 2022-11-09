091501038e610c82adf4ca152d185f4d49a7284e
KewWM <117059354+KewWM@users.noreply.github.com>
2022-11-09 19:17:43 +0800

    New

--- Lab 2/l2q1.java
+++ Lab 2/l2q1.java
@@ -10,7 +10,7 @@ public class l2q1 {
         System.out.print("Please enter temperature in degree Fehrenheit: ");
         int Feh = walao.nextInt();
         double Cel = (Feh-32)/1.8;
-        System.out.println("Temperature: " + Cel + " degree Celsius");
+        System.out.println("Temperature: " + String.format("%.2f",Cel) + " degree Celsius");
     }
     
 }
